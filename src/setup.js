fb5e161d14e9bf6029d7be773666eb8f398c3041
Dmitry Pimenov <dmpime@microsoft.com>
2017-09-22 08:51:22 -0700

    support for arguments for collections

--- src/setup.js
+++ src/setup.js
@@ -36,7 +36,7 @@ const version = 'v1.0';
 fetchMetadata();
 
 function fetchMetadata(){
-    request(endpoint+version+'/$metadata', function (error, response, body) {
+    request('https://raw.githubusercontent.com/microsoftgraph/MSGraph-SDK-Code-Generator/master/metadata/v1.0_2017_08_04_descriptions.xml', function (error, response, body) {
         if (!error){
             parseMetadata(body);
         }
@@ -54,10 +54,12 @@ function parseMetadata(metadata){
     let entities = entityResponse['entityMapping']
     let entityDependence = entityResponse['entityDependence'];
     let entityContainer = schema['EntityContainer'];
+    let entityAnnotations = entityResponse['entityAnnotation'];
+    let complexAnnotations = complexResponse['complexAnnotation'];
     let entitySets = parseEntitySets(entityContainer['EntitySet']); //mapping from name to type of root level entity set 
     let singletons = parseSingletons(entityContainer['Singleton']); //mapping from name to type of root level singletons
     let dependence = _.merge(complexDependence, entityDependence);
-    generator(dependence, entities, complexTypes, enums, entitySets, singletons);
+    generator(dependence, entities, complexTypes, enums, entitySets, singletons, entityAnnotations, complexAnnotations);
 }
 
 function parseEnums(enums){
@@ -78,6 +80,7 @@ function parseEnums(enums){
 
 function parseComplex(complex){
     let complexMapping = {};
+    let complexAnnotationMapping = {};
     let complexDependence = {};
     for (let complexType of complex){
         let name = complexType['_attributes']['Name'];
@@ -87,29 +90,42 @@ function parseComplex(complex){
             complexDependence[name] = convertTypeToGQL(false, baseType);
         }
         let mapping = {};
+        let annotationMapping = {};
         if (properties != null && properties.length > 0){
             for (let property of properties){
                 let name = property['_attributes']['Name'];
                 let type = property['_attributes']['Type'];
                 mapping[name] = type;
+                let annotation = null;
+                if (property['Annotation']){
+                    annotation = property['Annotation']['_attributes']['String'];
+                }
+                annotationMapping[name] = annotation;
             }
         } else if (properties != null){
             let name = properties['_attributes']['Name'];
             let type = properties['_attributes']['Type'];
             mapping[name] = type;
+            let annotation = null;
+                if (properties['Annotation']){
+                    annotation = properties['Annotation']['_attributes']['String'];
+                }
+            annotationMapping[name] = annotation;
         } else {
-            //lie if the complex type is empty
             mapping['extension'] = 'String'
         }
+        complexAnnotationMapping[name] = annotationMapping;
         complexMapping[name] = mapping;
     }
     return {"complexMapping": complexMapping,
-            "complexDependence": complexDependence};
+            "complexDependence": complexDependence,
+            "complexAnnotation": complexAnnotationMapping};
 }
 
 function parseEntities(entities){
     let entityDependence = {}; //key is child type
     let entityMapping = {};
+    let entityAnnotationMapping = {};
     for (let entityType of entities){
         let name = entityType['_attributes']['Name'];
         let baseType = entityType['_attributes']['BaseType'];
@@ -119,11 +135,17 @@ function parseEntities(entities){
             entityDependence[name] = convertTypeToGQL(false, baseType);
         }
         let mapping = {};
+        let annotationMapping = {};
         if (properties != null && properties.length > 0){
             for (let property of properties){
                 let name = property['_attributes']['Name'];
                 let type = property['_attributes']['Type'];
                 mapping[name] = type;
+                let annotation = null;
+                if (property['Annotation']){
+                    annotation = property['Annotation']['_attributes']['String'];
+                }
+                annotationMapping[name] = annotation;
             }
         }
         if (navigationProperties != null && navigationProperties.length > 0){
@@ -131,13 +153,20 @@ function parseEntities(entities){
                 let name = navigationProperty['_attributes']['Name'];
                 let type = navigationProperty['_attributes']['Type'];
                 mapping[name] = type;
+                let annotation = null;
+                if (navigationProperty['Annotation']){
+                    annotation = navigationProperty['Annotation']['_attributes']['String'];
+                }
+                annotationMapping[name] = annotation;
             }
         }
         mapping['id'] = 'ID';
-        entityMapping[name] = mapping
+        entityAnnotationMapping[name] = annotationMapping;
+        entityMapping[name] = mapping;
     }
     return {"entityMapping": entityMapping,
-            "entityDependence": entityDependence};
+            "entityDependence": entityDependence,
+            "entityAnnotation": entityAnnotationMapping};
 }
 
 function parseEntitySets(entitySets){
@@ -160,13 +189,13 @@ function parseSingletons(singletons){
     return singletonMapping;
 }
 
-function generator(dependence, entities, complexTypes, enums, entitySets, singletons){
+function generator(dependence, entities, complexTypes, enums, entitySets, singletons, entityAnnotations, complexaAnnotations){
     enumDefs = generateEnumDefs(enums);
-    entityDefs = generateTypeDefs(dependence, entities);
-    complexDefs = generateTypeDefs(dependence, complexTypes);
+    entityDefs = generateTypeDefs(dependence, entities, entityAnnotations);
+    complexDefs = generateTypeDefs(dependence, complexTypes, complexaAnnotations);
     queryDefs = generateQueryDefs(entitySets, singletons);
     let defs = _.union(enumDefs, entityDefs);
-    defs = _.union(defs,complexDefs);
+    defs = _.union(defs, complexDefs);
     defs = _.union(defs, queryDefs);
     defs = _.union(defs, additionalScalarTypes);
     const helperStr = generateHelperStr();
@@ -189,9 +218,10 @@ function codeGen(helperStr, schemaStr, resolverStr){
 function generateQueryDefs(entitySets, singletons){
     entitySets = typeAdjustments(entitySets, true, {}, {}, null);
     singletons = typeAdjustments(singletons, false, {}, {}, null);
-    let defs = _.merge(entitySets, singletons);
-    let subDefinitions = Object.keys(defs).map((key)=>' '+key+': '+defs[key]+' ');
-    let queryStr = '\ntype Query {\n'+subDefinitions.join('\n')+'\n}'
+    let entitySetSubDefinitions = Object.keys(entitySets).map((key)=>' '+key+'(id: ID): '+entitySets[key]+' ');
+    let singletonSubDefinitions = Object.keys(singletons).map((key)=>' '+key+': '+singletons[key]+' ');
+    let subDefs = entitySetSubDefinitions.concat(singletonSubDefinitions);
+    let queryStr = '\ntype Query {\n'+subDefs.join('\n')+'\n}'
     return [queryStr];
 }
 
@@ -206,12 +236,24 @@ function generateEnumDefs(enums){
     return defs;
 }
 
-function generateTypeDefs(dependence, types){
+function generateTypeDefs(dependence, types, annotations){
     let defs = [];
     for (let typeName of Object.keys(types)){
         let type = types[typeName];
         let adjustedTypes = typeAdjustments(type, false, types, dependence, typeName);
-        let subDefinitions = Object.keys(adjustedTypes).map((key)=>' '+key+': '+adjustedTypes[key]+'');
+        let subDefinitions = Object.keys(adjustedTypes).map((key)=>{
+            let name = key;
+            let value = adjustedTypes[key];
+            let isCollection = value.includes('[') && value.includes(']');
+            let isEntityCollection = isCollection && checkIfEntity(value.replace('[', '').replace(']', ''), types, dependence);
+            if (isEntityCollection){
+                name = name + "(id: ID)"
+            }
+            if (annotations[typeName] && annotations[typeName][key]){
+                return '#'+annotations[typeName][key]+'\n '+name+': '+value+''
+            } else {
+                return ' '+name+': '+value+''
+        }});
         let typeStr = '\ntype ' + typeName+' {\n'+subDefinitions.join('\n')+'\n} '
         defs.push(typeStr);
     }
@@ -227,20 +269,35 @@ function typeAdjustments(obj, implicitCollection, typeMapping, dependence, name)
         }
     }
     if (Object.keys(dependence).length > 0){
-        //first value
         let currentDependency = dependence[name];
         while (currentDependency != null){
-            //1 - pull in
-            let entityDef = typeMapping[currentDependency];
-            for (let propertyName of Object.keys(entityDef)){
-                let propertyValue = entityDef[propertyName];
+            let typeDef = typeMapping[currentDependency];
+            for (let propertyName of Object.keys(typeDef)){
+                let propertyValue = typeDef[propertyName];
                 mapping[propertyName] = convertTypeToGQL(implicitCollection, propertyValue);
             }
-            //2 - go to next
             currentDependency = dependence[currentDependency];
         }
     } 
     return mapping;
+}
+
+function checkIfEntity(name, typeMapping, dependence){
+    if (Object.keys(dependence).length > 0){
+        let currentDependency = dependence[name];
+        while (currentDependency != null){
+            let typeDef = typeMapping[currentDependency];
+            if (typeDef != null){
+                if (Object.keys(typeDef).includes('id')){
+                    return true;
+                }
+                currentDependency = dependence[currentDependency];
+            } else {
+                break;
+            }
+        }
+    } 
+    return false;
 }
 
 function convertTypeToGQL(implicitCollection, propertyValue){
@@ -284,7 +341,7 @@ function generateQueryDefResolvers(entitySets, singletons){
     ` ${key} (obj, args, context){ 
         const name = \'${key}\';
         const requestUrl = graphRoot + "/" + name;
-        return makeRequest(requestUrl, obj['__session'], false);
+        return makeRequest(requestUrl, obj['__session'], args, false);
     }`);
     let queryStr = `\nQuery: {\n ${subDefinitions.join(',\n')} \n }`;
     return [queryStr];
@@ -304,7 +361,7 @@ function generateTypeDefResolversHelper(typeCollection, dependence, defs){
         let subDefinitions = Object.keys(adjustedTypes).map((key)=>
             `   ${key} (obj, args, context){
                 const name = \'${key}\';
-                return graphqlResolve(obj, name);\n  }\n`);
+                return graphqlResolve(obj, name, args);\n  }\n`);
         let typeStr = `\n${typeName}: {\n ${subDefinitions.join(',\n')}} `;
         defs.push(typeStr);
     }
@@ -312,22 +369,27 @@ function generateTypeDefResolversHelper(typeCollection, dependence, defs){
 
 function generateHelperStr(){
     return `
-    const graphRoot = \'${endpoint+version}\'; 
+    const graphRoot = \'${endpoint+version}\';
+    const urlRoot = "https://proxy.apisandbox.msdn.microsoft.com/svc?url="
     const request = require('request-promise');
     module.exports = {
         typeDefs,
         resolvers
     }
-    function makeRequest(path, authorization, isSecondary) {
+    function makeRequest(path, authorization, args, isSecondary) {
+        let url = urlRoot + encodeURIComponent(path);
+        if (args.id != null) { 
+            url = url + '/' + args.id;
+        }
         var options = {
-            url: path,
+            url: url,
             headers: {
-                'Authorization': 'bearer ' + authorization
+                'Authorization': 'Bearer ' + authorization
             }
         };
-        return request(options).then(function (requestBody, error) {
+        return request(options).then(function (responseBody, error) {
             if (!error) {
-                let body = JSON.parse(requestBody);
+                let body = JSON.parse(responseBody);
                 if (Object.keys(body).includes('value')) {
                     var next = body['value'];
                     if (Array.isArray(next)) {
@@ -341,10 +403,17 @@ function generateHelperStr(){
                     }
                     return next;
                 } else {
-                    body['__path'] = path;
                     body['__session'] = authorization;
                     if (isSecondary) {
                         body['__secondary'] = true;
+                    }
+                    if (args.id != null){
+                        body['__path'] = path+'/'+args.id;
+                        if (!Array.isArray(body)){
+                            body = [body];                        
+                        }
+                    } else {
+                        body['__path'] = path;
                     }
                     return body;
                 }
@@ -352,10 +421,12 @@ function generateHelperStr(){
                 console.log(error);
                 return 'error';
             }
-        });
+        }).catch(function(err){
+		    return {}; // return empty object 
+	    });
     }
-    function graphqlResolve(obj, name){
-        return parseOrRequest(obj, name).then(response => {
+    function graphqlResolve(obj, name, args){
+        return parseOrRequest(obj, name, args).then(response => {
             if (Object.keys(response).includes('value')) {
                 var next = response['value'];
                 if (Array.isArray(next)) {
@@ -363,14 +434,18 @@ function generateHelperStr(){
                 }
                 return next;
             } else if (Object.keys(response).includes('__secondary') || Array.isArray(response)) {
-                return response;
+                if (Object.keys(response).includes('@odata.null')){
+				    return (response['@odata.null'] == true) ? null : response;
+                } else {
+                    return response;
+                }
             } else {
                 return response[name];
             }
         });
     }
 
-    function parseOrRequest(obj, currentProperty) {
+    function parseOrRequest(obj, currentProperty, args) {
         const nextPath = obj.__path + '/' + currentProperty;
         if (Object.keys(obj).includes(currentProperty)) {
             return new Promise((resolve, reject) => {
@@ -382,7 +457,7 @@ function generateHelperStr(){
             });
         } else {
             let auth = obj['__session'];
-            let resp = makeRequest(nextPath, auth, true);
+            let resp = makeRequest(nextPath, auth, args, true);
             return resp;
         }
     }
